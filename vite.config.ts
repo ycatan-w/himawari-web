01b48fa0ae860b68a1f1bac08a7978573a8bc0a2
ycatan-w <225661717+ycatan-w@users.noreply.github.com>
2025-08-15 20:44:50 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build'
+
+  return {
+    base: !isBuild ? '/' : '/himawari-web/',
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
 })
